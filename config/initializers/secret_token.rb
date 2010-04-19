aa1b038737a7474f10dfff5dbae3330a7ca79751
rngtng <tobi@rngntg.com>
2010-04-19 19:47:10 +0200

    initial commit, raw rails installation with mysql, twitter and capistrano dependencies

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '7bb407b20fb552945806fa66e3186722f78ee79ee19397921b9e2e46cd705aa1e26e5d3bc05925e7a6f7a1d57dbb1474dad0b0ce3b3fad9a9817d8c3a2e439ff'
