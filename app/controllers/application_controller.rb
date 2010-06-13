a01036f5b35d865466f9328138d185364f145eb3
rngtng <tobi@rngntg.com>
2010-06-13 16:11:28 +0200

    added access denied page, added devise hack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
   
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :unless => proc { |c| c.devise_controller? } #TODO remove this hack!!
   
   def authenticate_admin!
-    render_404 unless current_user.admin?
+    render_403 unless current_user.admin?
   end
   
+  def render_403
+    render :file => "public/404", :layout => false, :status => 403
+  end
+
   def render_404
     render :file => "public/404", :layout => false, :status => 404
   end
