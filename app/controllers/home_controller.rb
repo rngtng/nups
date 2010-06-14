629b4bd5db97e3f3bc296d4039caa939ed366c6a
rngtng <tobi@rngntg.com>
2010-06-14 14:50:56 +0200

    updated menu to show newsletters for selected user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,8 @@ class HomeController < ApplicationController
   def menu
     @user = current_user
     @user = User.find(params[:user_id]) if params[:user_id] && current_user.admin?
+    
+    @users = current_user.admin? ? User.all : Array(@user)
     render :layout => false
   end
 
