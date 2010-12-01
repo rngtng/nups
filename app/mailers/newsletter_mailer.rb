a493381f4578ada48190d83003de52c4fe5e92b1
Tobias Bielohlawek <tobi@soundcloud.com>
2010-12-01 01:48:12 +0100

    fixed mail headers

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -3,28 +3,29 @@ class NewsletterMailer < ActionMailer::Base
   def issue(newsletter, recipient)
     default_url_options[:host] = newsletter.host
 
-    head = {}
-
     template_html  = newsletter.template_html.blank? ? "<%= content %>" : newsletter.template_html
     template_text  = newsletter.template_text.blank? ? "<%= content %>" : newsletter.template_text
 
+    head = {}
     head[:to]      = recipient.email
     head[:from]    = newsletter.from
+
+    head[:sender]  = "error@c-art-web.de"
 
     head[:subject] = newsletter.subject
     head[:subject] = "TEST: #{subject}" if newsletter.test?
 
     head["X-Sender"] = "MultiAdmin"
-    head["X-MA-Account-Id"] = newsletter.account.id
+    head["X-MA-Account-Id"] = newsletter.account.id.to_s
 
     mail(head) do |format|
       data = { :subject => subject, :content => newsletter.content.to_s.html_safe, :newsletter => newsletter, :recipient => recipient }
-      if newsletter.has_html?
-        format.html { render :inline => template_html, :locals => data }
-      end
-
       if newsletter.has_text?
         format.text { render :inline => template_text, :locals => data }
+      end
+
+      if newsletter.has_html?
+        format.html { render :inline => template_html, :locals => data }
       end
     end
 
