b0ca5f9738ec6d6780c35a13b07f6a65f56076ba
Tobias Bielohlawek <tobi@soundcloud.com>
2010-12-01 01:22:59 +0100

    added mailer header

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -3,16 +3,29 @@ class NewsletterMailer < ActionMailer::Base
   def issue(newsletter, recipient)
     default_url_options[:host] = newsletter.host
 
-    template_html = newsletter.template_html.blank? ? "<%= content %>" : newsletter.template_html
-    template_text = newsletter.template_text.blank? ? "<%= content %>" : newsletter.template_text
+    head = {}
 
-    subject = newsletter.subject
-    subject = "TEST: #{subject}" if newsletter.test?
+    template_html  = newsletter.template_html.blank? ? "<%= content %>" : newsletter.template_html
+    template_text  = newsletter.template_text.blank? ? "<%= content %>" : newsletter.template_text
 
-    mail :to => recipient.email, :subject => subject, :from => newsletter.from do |format|
+    head[:to]      = recipient.email
+    head[:from]    = newsletter.from
+
+    head[:subject] = newsletter.subject
+    head[:subject] = "TEST: #{subject}" if newsletter.test?
+
+    head["X-Sender"] = "MultiAdmin"
+    head["X-MA-Account-Id"] = newsletter.account.id
+
+    mail(head) do |format|
       data = { :subject => subject, :content => newsletter.content.to_s.html_safe, :newsletter => newsletter, :recipient => recipient }
-      format.text { render :inline => template_text, :locals => data } if newsletter.has_text?
-      format.html { render :inline => template_html, :locals => data } if newsletter.has_html?
+      if newsletter.has_html?
+        format.html { render :inline => template_html, :locals => data }
+      end
+
+      if newsletter.has_text?
+        format.text { render :inline => template_text, :locals => data }
+      end
     end
 
     newsletter.attachments.each do |attachment|
