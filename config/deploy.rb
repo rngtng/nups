860aaccc90adb92fd5ffa67e7336d8160d8e0c40
Tobias Bielohlawek <tobi@soundcloud.com>
2011-02-14 21:22:52 +0100

    updated to latest deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,16 @@
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+
+require 'rvm/capistrano'
+require 'bundler/capistrano'
+
 set :application, "nups"
 
 set :use_sudo, false
-set :rvm_type, :user
+set :user, 'ssh-21560'
 
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-require "rvm/capistrano"
-set :rvm_ruby_string, 'ruby-1.9.2-rc2'
+set :rvm_type, :user
+set :rvm_ruby_string, "ruby-1.9.2-rc2@#{application}"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -15,17 +20,17 @@ set :deploy_to, "/kunden/warteschlange.de/produktiv/rails/#{application}"
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 set :scm, :git
-set :repository, "git://github.com/rngtng/nups.git"
+set :repository, "git://github.com/rngtng/#{application}.git"
 set :branch, "master"
+
 set :deploy_via, :remote_cache
 
-set :user, 'ssh-21560'
-set :ssh_options, { :forward_agent => true }
+set :ssh_options, :forward_agent => true
 
-role :app, "www2.multiadmin.de"
-role :web, "www2.multiadmin.de"
-role :db,  "www2.multiadmin.de", :primary => true
-role :job, "www2.multiadmin.de"
+role :app, "#{application}.warteschlange.de"
+role :web, "#{application}.warteschlange.de"
+role :db,  "#{application}.warteschlange.de", :primary => true
+role :job, "#{application}.warteschlange.de"
 
 namespace :deploy do
   desc "Restarting mod_rails with restart.txt"
@@ -33,52 +38,25 @@ namespace :deploy do
     run "touch #{current_path}/tmp/restart.txt"
   end
 
-  desc "Link in the production database.yml" 
+  desc "Link in the production database.yml"
   task :link_configs do
     run "ln -nfs #{deploy_to}/#{shared_dir}/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{deploy_to}/#{shared_dir}/mail.yml #{release_path}/config/mail.yml"
     run "ln -nfs #{deploy_to}/#{shared_dir}/uploads #{release_path}/public/system"
-  end  
-  
-  
+  end
+
   [:start, :stop].each do |t|
     desc "#{t} task is a no-op with mod_rails"
     task t, :roles => :app do ; end
   end
 end
 
-## Rails 3 updates:  http://rand9.com/blog/bundler-0-9-1-with-capistrano
-namespace :bundler do
-  task :create_symlink, :roles => :app do
-    shared_dir = File.join(shared_path, 'bundle')
-    release_dir = File.join(current_release, '.bundle')
-    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
-  end
-  
-  task :bundle_new_release, :roles => :app do
-    bundler.create_symlink
-    run "cd #{release_path} && bundle install --without test"
-  end
-  
-  task :lock, :roles => :app do
-    run "cd #{current_release} && bundle lock;"
-  end
-  
-  task :unlock, :roles => :app do
-    run "cd #{current_release} && bundle unlock;"
-  end
-end
-
-def remote_file_exists?(full_path)
-  'true' ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
-end
-
 namespace :resque do
   resque_pid = File.join(current_release,"tmp/pids/resque_worker.pid")
   resque_log = "log/resque_worker.log"
-  
+
   desc "start all resque workers"
-  task :start, :roles => :job do    
+  task :start, :roles => :job do
     unless remote_file_exists?(resque_pid)
       run "cd #{release_path}; RAILS_ENV=production QUEUE=* VERBOSE=1 nohup rake resque:work &> #{resque_log}& 2> /dev/null && echo $! > #{resque_pid}"
     else
@@ -106,11 +84,7 @@ namespace :resque do
   end
 end
 
-
-
-# HOOKS
 after "deploy:update_code" do
-  bundler.bundle_new_release
   deploy.link_configs
 end
 
