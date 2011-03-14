96e35e8f8b33e8759c9b1639846c7b8c29186d15
Tobias Bielohlawek <tobi@soundcloud.com>
2011-03-15 00:55:37 +0100

    moved from frame to iframe layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,11 @@
 class HomeController < ApplicationNupsController
 
   def index
-    render :layout => false
-  end
-
-  def top_menu
-    render :layout => false
-  end
-
-  def menu
     @user = current_user
     @user = User.find(params[:user_id]) if params[:user_id] && current_user.admin?
 
     @users = current_user.admin? ? User.all : Array(@user)
-    render :layout => false
-  end
 
-  def main
+    render :layout => false
   end
 end
