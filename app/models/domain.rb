8a7938640750d9a6d3732e4f8ab615ebd6d6a0e4
Tobias Bielohlawek <tobi@soundcloud.com>
2011-04-19 00:38:46 +0200

    link to main page

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,12 +1,12 @@
 class Domain < ActiveRecord::Base
   belongs_to :user
 
-  def email_url
-    "https://sslsites.de/kundenmenue/email.php?rekm_login=#{username}&rekm_password=#{password}&dn=#{number}"
+  def main_url
+    "https://sslsites.de/kundenmenue/uebersicht.php?rekm_login=#{username}&rekm_password=#{password}"
   end
 
   def ftp_url
-    "https://sslsites.de/kundenmenue/ftp.php?rekm_login=#{username}&rekm_password=#{password}&dn=#{number}"
+    "https://sslsites.de/kundenmenue/ftp.php?rekm_login=#{username}&rekm_password=#{password}"
   end
 
 end
