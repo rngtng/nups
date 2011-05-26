7a172b48d9b4233962524e4986b3b6ee287dd0fb
Tobias Bielohlawek <tobi@soundcloud.com>
2011-05-26 19:11:51 +0200

    added bounce parsing

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,5 @@
+require 'net/imap'
+
 class Account < ActiveRecord::Base
 
   belongs_to :user
@@ -39,6 +41,44 @@ class Account < ActiveRecord::Base
     @reply_to ||= (self.mail_config && self.mail_config['reply_to']) || self.from_email
   end
 
+  def process_bounces
+    mail_settings = mail_config['smtp_settings']
+
+    imap = Net::IMAP.new(mail_settings[:address].gsub('smtp', 'imap'))
+    imap.authenticate('LOGIN', mail_settings[:user_name], mail_settings[:password])
+    imap.select('INBOX') #use examaine fpr read only
+
+    # all msgs
+    sources = {}
+    imap.uid_search(["SINCE", "1-Jan-1969", "NOT", "DELETED"]).each do |id|
+      out = "BOUNCE #{id}"
+      begin
+        mail = Mail.new imap.uid_fetch(id, ['RFC822']).first.attr['RFC822']
+        if mail.bounced?
+          mail_id = mail.body.match(/X-MA-Id:? ?([^\r\n ]+)/)[1]
+          out << " - bounced id: #{mail_id}"
+          dummy, account_id, newsletter_id, recipient_id = mail_id.split('-')
+          if r = Recipient.find_by_account_id_and_id(account_id, recipient_id)
+            out << " - found"
+            rec = mail.final_recipient.split(";")[1].strip
+            unless r.bounces.to_s.include?(mail_id)
+              r.bounces_count += 1
+              r.bounces = "#{mail.date.strftime("%Y-%m-%d")} #{mail_id} <#{rec}>: #{mail.error_status} #{mail.diagnostic_code}\n#{r.bounces}"
+              r.save!
+              out << " - saved"
+            end
+            imap.uid_store(id, "+FLAGS", [:Deleted])
+          end
+        end
+      rescue => e
+        logger.warn " -------> error on #{id} #{e.message}"
+      end
+      logger.info out
+    end
+    imap.expunge
+    imap.close
+    imap.logout
+  end
 end
 
 # == Schema Info
