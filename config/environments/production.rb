9e768dc261676a37cfdc20b5874307be4d1caef0
Tobias Bielohlawek <tobi@soundcloud.com>
2011-07-21 21:22:06 +0200

    updated latest gems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Nups::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  # config.action_controller.asset_host = "http://www2.multiadmin.de:8080"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( frame.js )
