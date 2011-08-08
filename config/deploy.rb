ee22e14278098215492da49b236eef50db3b0530
Tobias Bielohlawek <tobi@soundcloud.com>
2011-08-08 19:39:46 +0200

    updated gems, fixed asset precompile path

--- config/deploy.rb
+++ config/deploy.rb
@@ -74,8 +74,7 @@ namespace :deploy do
 
   desc "precompile the assets"
   task :precompile_assets, :roles => :web, :except => { :no_release => true } do
-    run "cd #{current_path}; rm -rf public/assets/*"
-    run "cd #{current_path}; RAILS_ENV=production bundle exec rake assets:precompile"
+    run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
   end
 end
 
