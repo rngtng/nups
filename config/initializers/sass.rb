599612ac45213b364a643216dccbefd6bbab47fc
Tobias Bielohlawek <tobi@soundcloud.com>
2011-08-22 23:41:34 +0200

    enable sass helpers

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -2,3 +2,5 @@
 # Rails.configuration.sass.tap do |config|
 #   config.load_paths << "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/compass/stylesheets"
 # end
+
+require 'sass/rails/helpers'
