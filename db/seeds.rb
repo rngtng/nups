0017c09a881e27d7b7ffc190bdc09b9b17ffeb0c
Tobias Bielohlawek <tobi@soundcloud.com>
2011-09-20 17:26:22 +0200

    added seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,29 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+User.delete_all
+user = User.new(username:'admin', email:'admin@localhost.de')
+user.password = user.password_confirmation = 'admin'
+user.admin = true
+user.save!
 
-# user = User.create(:username => 'admin', :email => 'admin@localhost.de')
-# user.admin = true
-# user.save!
+Account.delete_all
+account = Account.new(name:"Test", from:"test@localhost.de", user:user, subject:"Test", has_html:true, has_text:true, has_attachments:true, has_scheduling:true)
+account.template_html = account.template_text = "<%= content >"
+account.save!
+
+newsletters = {}
+Newsletter.delete_all
+Newsletter.new.state_paths.to_states.each do |state|
+  newsletters[state] = Newsletter.create(subject:"Test in #{state}", content:"Test", account:account, state:state.to_s)
+end
+
+newsletters[:sending].update_attributes(deliveries_count:20, recipients_count:100)
+newsletters[:stopping].update_attributes(deliveries_count:60, recipients_count:100)
+newsletters[:stopped].update_attributes(deliveries_count:80, recipients_count:100)
+newsletters[:finished].update_attributes(deliveries_count:100, recipients_count:100)
+
+Recepient.delete_all
+100.times do |index|
+  Recepient.create(email:"test-#{index}@localhost.de", account:account)
+end
