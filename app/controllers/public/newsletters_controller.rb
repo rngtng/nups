ab585227d7fb54f68e06a40025218a1fdce1d8bc
Tobias Bielohlawek <tobi@soundcloud.com>
2011-12-08 14:46:21 +0100

    moves images to right folder

--- app/controllers/public/newsletters_controller.rb
+++ app/controllers/public/newsletters_controller.rb
@@ -9,7 +9,7 @@ class Public::NewslettersController < ApplicationController
       respond_with do |format|
         format.html { @issue = @send_out.issue }
         # http://stackoverflow.com/questions/5228238/rails-how-to-send-an-image-from-a-controller
-        format.gif { send_file "public/pixel.gif" }
+        format.gif { send_file "public/images/pixel.gif" }
       end
     else
       render_404
