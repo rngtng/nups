4a83dbbfcf7545e075f1b0ba1ec863e1196ba774
Tobias Bielohlawek <tobi@soundcloud.com>
2011-12-26 00:37:24 +0100

    added chart to analyze read behavior

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -63,4 +63,51 @@ module ChartsHelper
       ORDER BY `date`
     SQL
   end
+
+  def send_outs_chart(account)
+     opts = {
+      15.minutes => "&lt; 15min",
+      1.hour     => "15min - 1h",
+      3.hours    => "1h - 3h",
+      12.hours   => "3h - 12h",
+      1.day      => "12h - 24h",
+      2.day      => "24h - 48h",
+      100.days   => "&gt; 48h"
+    }
+    newsletter_ids = account.newsletters.map(&:id)
+    {
+      :read_diff => {
+        :opts => opts,
+        :data => get_read_diff(newsletter_ids, opts),
+      },
+      :read_span => get_read_span(newsletter_ids),
+    }
+  end
+
+  def get_read_diff(newsletter_ids, opts)
+    return [] unless newsletter_ids.any?
+    min = opts.keys.min
+    Recipient.connection.select_rows <<-SQL
+      SELECT TIME_TO_SEC(TIMEDIFF(updated_at, IFNULL(finished_at, created_at))) DIV #{min} * #{min} AS `date`,
+      COUNT(*)
+      FROM send_outs
+      WHERE state = 'read'
+      AND newsletter_id IN(#{newsletter_ids.join(',')})
+      GROUP BY `date`
+    SQL
+  end
+
+  def get_read_span(newsletter_ids, span = 10.minutes)
+    return [] unless newsletter_ids.any?
+    Recipient.connection.execute("Set time_zone = '+0:00'")
+    Recipient.connection.select_rows <<-SQL
+      SELECT
+      UNIX_TIMESTAMP(DATE_FORMAT(updated_at, "2011-01-01 %H:%i")) DIV #{span} AS `date`,
+      COUNT(*)
+      FROM send_outs
+      WHERE state = 'read'
+      AND newsletter_id IN(#{newsletter_ids.join(',')})
+      GROUP BY `date`
+    SQL
+  end
 end
